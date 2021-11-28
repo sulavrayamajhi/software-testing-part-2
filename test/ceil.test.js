8bd686893328ad4f42b94102774965eba614aa3d
Nikesh Adhikari <nikesh.adhikari@tuni.fi>
2021-11-28 18:59:41 +0200

    [ADD]: Test for Ceil.js

---
+++ test/ceil.test.js
@@ -0,0 +1,13 @@
+import chai from 'chai';
+import ceil from '../src/ceil.js';
+const expect = chai.expect;
+
+describe("Ceil - Round up number to precision", () => {
+    it('Convert the non-integer to number with 2 digit decimal accuracy', () => {
+        expect(ceil(2.4569, 2)).to.equal(2.46)
+    })
+
+    it('Converting the integer to number with 2 digit decimal accuracy', () => {
+        expect(ceil(2, 2)).to.equal(2.00)
+    })  
+})
