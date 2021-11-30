051445c1594d33648dfa7931b2615fade1c91b93
Nikesh Adhikari <nikesh.adhikari@tuni.fi>
2021-11-30 14:51:09 +0200

    [Modify]: test input added on ceil

--- test/ceil.test.js
+++ test/ceil.test.js
@@ -5,8 +5,9 @@ const expect = chai.expect;
 describe("Ceil - Round up number to precision", () => {
     it('Convert the non-integer to number with 2 digit decimal accuracy', () => {
         expect(ceil(2.4569, 2)).to.equal(2.46)
+        expect(ceil(2.62, 2)).to.equal(2.62)
     })
-
+    
     it('Converting the integer to number with 2 digit decimal accuracy', () => {
         expect(ceil(2, 2)).to.equal(2.00)
     })  
